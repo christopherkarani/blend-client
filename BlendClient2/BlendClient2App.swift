66ba6ca4f610c3e6073a54f5c80cf7a676f68964
christopherkarani <chrisbkarani@gmail.com>
2025-05-18 15:50:49 +0300

    Initial Commit

---
+++ BlendClient2/BlendClient2App.swift
@@ -0,0 +1,17 @@
+//
+//  BlendClient2App.swift
+//  BlendClient2
+//
+//  Created by Chris Karani on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BlendClient2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlendClient2Tests/BlendClient2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BlendClient2Tests.swift
+//  BlendClient2Tests
+//
+//  Created by Chris Karani on 18/05/2025.
+//
+
+import Testing
+@testable import BlendClient2
+
+struct BlendClient2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
